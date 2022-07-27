efd13b5e30aa902f046b2aae2ea0dd9669d0a9a1
vibhav chirravuri <44679811+csvultrainstinct@users.noreply.github.com>
2022-07-27 20:13:14 +0530

    Create MainCode.cpp

---
+++ LinkedLists&Trees/MainCode.cpp
@@ -0,0 +1,111 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class TreeNode{
+    public:
+    int val;
+    TreeNode*left=NULL;
+    TreeNode*right=NULL;
+    TreeNode(int data){
+        val=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+class ListNode{
+    public:
+    int val;
+    ListNode*next=NULL;
+    ListNode(int data){
+        val=data;
+        next=NULL;
+    }
+};
+
+TreeNode*insertNode(TreeNode*root,int data){
+    if(root==NULL){
+        root=new TreeNode(data);
+        return root;
+    }
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode*temp=q.front();
+        q.pop();
+        if(temp->left!=NULL){
+            q.push(temp->left);
+        }
+        else{
+            temp->left=new TreeNode(data);
+            return root;
+        }
+        if(temp->right!=NULL){
+            q.push(temp->right);
+        }
+        else{
+            temp->right=new TreeNode(data);
+            return root;
+        }
+        
+    }
+}
+void inorder(TreeNode*root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->val<<" ";
+    inorder(root->right);
+}
+
+ListNode*insertListNode(ListNode*head,int data){
+    if(head==NULL){
+        head=new ListNode(data);
+        return head;
+    }
+    ListNode*temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=new ListNode(data);
+    
+    return head;
+}
+
+void PrintList(ListNode* head){
+    ListNode*temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    TreeNode*root=NULL;
+    
+    for(auto i=0;i<n;i++){
+       int data;
+       cin>>data;
+       root=insertNode(root,data);
+    }
+    
+    inorder(root);
+
+    ListNode*head=NULL;
+    for(auto i=0;i<n;i++){
+        int data;
+        cin>>data;
+        head=insertListNode(head,data);
+    }
+    
+    PrintList(head);
+    return 0;
+}
